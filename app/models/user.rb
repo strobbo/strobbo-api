17e234130fb733b24527fd9d8f2d22d810c4a6da
felipeecst <felipeecst@gmail.com>
2013-07-23 02:18:53 -0300

    Acesso a lista de amigos

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ActiveRecord::Base
   devise :rememberable, :trackable, :omniauthable, :omniauth_providers => [:facebook]
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :encrypted_password, :remember_me, :provider, :uid, :name, :image, :location
-  # attr_accessible :title, :body
+	attr_accessible :email, :encrypted_password, :remember_me, :provider, :uid, :name, :image, :location, 
+									:access_token
 
 	def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
 		user = User.where(:provider => auth.provider, :uid => auth.uid).first
@@ -17,6 +17,7 @@ class User < ActiveRecord::Base
 		                       email:auth.info.email,
 													 image:auth.info.image,
 													 location:auth.info.location,
+													 access_token:auth.credentials.token,
 		                       encrypted_password:Devise.friendly_token[0,20]
 		                       )
 		end
