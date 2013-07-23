b5a582039a949b0fc915be9250bf3fbb6cf2e6d5
felipeecst <felipeecst@gmail.com>
2013-07-23 01:50:44 -0300

    CRUD inicial de eventos

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Platform::Application.routes.draw do
 
+  resources :events
+
+
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 	devise_scope :user do
 		get 'sign_in', :to => 'devise/sessions#new', :as => :new_session
